c120700a4206adb82f437de3fed6808e57e37c34
Matej Dujič <matej.dujic@visionect.com>
2018-06-06 09:42:34 +0200

    commenting the exercise

--- exc5.py
+++ exc5.py
@@ -1,24 +1,25 @@
-# Instructions
 import time
 import random
-# Create a program that represents a football bet.
-# The program needs to have a list of football matches, where for every match user should input
-# the end result. There needs to be at least 5 matches. Example of input:
-# (programs output) Liverpool : CSKA ?
-# (user's input) 1-0
-# (programs output) Bate Borisov : Partizan  ?
-#  ....
+
 tekme = ["Brinje : Arne Tabor", "Zagorje : Ivancna Gorica", "Sencur : Komenda", "Kranj : Kolpa", "Kocevje : Rudar Trbovlje"]
 #
 tvoj_vnos = []
 for i in (tekme):
+      # this works fine, but since you dont need elements from tekme but only how many times to ask, this line would be appropriate:
+      # for i in range(len(tekme)):
       print(i)
       rezultat_tekme = (tvoj_vnos.append(input(str("Vnesi rezultat:"))))
+      # rezultat tekme is never used, you can totally omit it
+      # input returns string anyway so the line below does the same, but is much simpler
+#       tvoj_vnos.append(input("Vnesi rezultat:"))
+#     since max 5 goals can be scored by a team, user should know that so input more than  would be rejected or re-asked.
 
 #time.sleep(10)
 
 def generate_random_number():
     return (random.randint(0,5))
+# since you actually need string, you could do the string() casting here, so when you use it, you hide complexity.
+#   return str(random.randint(0,5))
 
 izidi = []
 import re
@@ -28,6 +29,8 @@ denar=0
 
 for i in range(len(tekme)):
     izidi.append(str(generate_random_number())+"-"+str(generate_random_number()))
+    # since you use generated result in the same loop, there is no need for izidi array.
+    # you could just save the generated result to variable and use this variable below.
     print("Actual game: " + tekme[i] +" "+ izidi[i])
     print("Your bet: " + tvoj_vnos[i])
     if (izidi[i][0] == tvoj_vnos[i][0]) and (izidi[i][2] == tvoj_vnos[i][2]):
@@ -36,15 +39,8 @@ for i in range(len(tekme)):
     else:
         denar -=1
         print("You lose -1€\n")
+
+
 print("Your wallet= " + str(denar)+"€")
-# After user inputs all the results, program waits for 10 seconds and prints the result of matches.
-# Results are totally random, you can say that max. goals by one team is 5.
-# After results are displayed, program should output, how well were the bets.
-# For every correctly guessed match, user gets 10€, for every incorrect user loses 1€
-# Example of output:
-# Actual game: Liverpool : CSKA 3-0.
-# Your bet: 1-0
-# You lose 1€
-# ....
-# --------------
-# You earned -5€.
+
+
